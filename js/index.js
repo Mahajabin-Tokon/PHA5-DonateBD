c31a64ffa977928ad996b9b3aefd51b74e462422
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-09-22 18:38:42 -0600

    HTML and Tailwind part mostly complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log("Hello world")
+// const blogBtn = document
+//   .getElementById("blog")
+//   .addEventListener("click", function () {
+//     console.log("hello")
+//     window.location.href = "./faq.html";
+//   });
+
+// const homeBtn = document
+//   .getElementById("home")
+//   .addEventListener("click", function () {
+//     console.log("hello")
+//     window.location.href = "./index.html";
+//   });
+
+function changePageToFAQ() {
+  window.location.href = "./faq.html";
+}
+
+function changePageToHome() {
+    window.location.href = "./index.html";
+  }
