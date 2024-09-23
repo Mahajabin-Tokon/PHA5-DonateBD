c024b512796b7e79e9f1629137e7f7fe1508a22e
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-09-22 21:09:41 -0600

    Modal added

--- js/index.js
+++ js/index.js
@@ -26,26 +26,29 @@ document
   .addEventListener("click", function (event) {
     event.preventDefault();
 
+    const histSectionEl = document.getElementById("history-section");
+
     const currentBalance = getTextFieldValueById("current-balance");
     const donateOneAmount = getTextFieldValueById("donate-one-amount");
+    const title = document.getElementById("donate-one-title").innerText;
+    
     const amount = getInputFieldValueById("donate-one-input");
-
-    console.log(amount);
 
     if (amount <= currentBalance && amount >= 0) {
       const newBalance = currentBalance - amount;
       document.getElementById("current-balance").innerText = newBalance;
 
-      const newDonationAmount = donateOneAmount + amount
-      document.getElementById("donate-one-amount").innerText = newDonationAmount;
+      const newDonationAmount = donateOneAmount + amount;
+      document.getElementById("donate-one-amount").innerText =
+        newDonationAmount;
 
-      // add to transaction history
-      //   const p = document.createElement("p");
-      //   p.innerText = `Added: ${addMoney} Tk. New Balance: ${newBalance}`;
-      //   console.log(p);
+      console.log(histSectionEl);
 
-      //   // should be a common function
-      //   document.getElementById("transaction-container").appendChild(p);
+      histSectionEl.innerHTML += `<div class="flex flex-col justify-center items-start p-4 mx-auto mt-10 w-11/12 border-2 rounded-lg">
+        <p class="text-xl p-4">${amount}Tk is donated for ${title}</p>
+        <p class="text-xl bg-slate-300 rounded-lg p-4 w-full">${new Date()}</p>
+      </div>`;
+
     } else if (amount <= 0) {
       alert("Donation amount cannot be negative");
     } else if (isNaN(amount)) {
