45dac73cb807b5cd9d67acd755c2592347f8ce8c
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-09-22 23:32:05 -0600

    Implemented sticky elements

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+// Scroll function
+const stickyNav = document.getElementById("scrolling-element");
+const nav = document.getElementById("navbar");
+
+// Use offsetTop to detect when it becomes sticky
+const stickLimit = stickyNav.offsetTop;
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > stickLimit) {
+    stickyNav.classList.add("bg-gray-600 bg-opacity-25 backdrop-blur-lg");
+    nav.classList.remove("bg-orange-50");
+    nav.classList.add("bg-gray-600 bg-opacity-25 backdrop-blur-lg");
+  } else {
+    stickyNav.classList.remove("bg-gray-600 bg-opacity-25 backdrop-blur-lg");
+    nav.classList.add("bg-orange-50");
+    nav.classList.remove("bg-gray-600 bg-opacity-25 backdrop-blur-lg");
+  }
+});
+
 // Changing HTML Pages
 function changePageToFAQ() {
   window.location.href = "./faq.html";
@@ -49,7 +68,7 @@ document
         <p class="text-xl p-4">${amount}Tk is donated for ${title}</p>
         <p class="text-xl bg-slate-300 rounded-lg p-4 w-full">${new Date()}</p>
       </div>`;
-      console.log(modal)
+      console.log(modal);
 
       modal.showModal();
     } else if (amount <= 0) {
