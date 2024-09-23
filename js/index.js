e132bfcd8fb53d7301e6ea825d591e2d73136eca
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-09-22 20:06:50 -0600

    Add money functionality added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Changing HTML Pages
 function changePageToFAQ() {
   window.location.href = "./faq.html";
 }
@@ -6,6 +7,7 @@ function changePageToHome() {
   window.location.href = "./index.html";
 }
 
+// Swtiching between Donation & History Section
 document
   .getElementById("donation-section-btn")
   .addEventListener("click", function () {
@@ -16,4 +18,39 @@ document
   .getElementById("history-section-btn")
   .addEventListener("click", function () {
     showSectionById("history-section");
+  });
+
+// Adding Donation Calculation
+document
+  .getElementById("donate-one-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const currentBalance = getTextFieldValueById("current-balance");
+    const donateOneAmount = getTextFieldValueById("donate-one-amount");
+    const amount = getInputFieldValueById("donate-one-input");
+
+    console.log(amount);
+
+    if (amount <= currentBalance && amount >= 0) {
+      const newBalance = currentBalance - amount;
+      document.getElementById("current-balance").innerText = newBalance;
+
+      const newDonationAmount = donateOneAmount + amount
+      document.getElementById("donate-one-amount").innerText = newDonationAmount;
+
+      // add to transaction history
+      //   const p = document.createElement("p");
+      //   p.innerText = `Added: ${addMoney} Tk. New Balance: ${newBalance}`;
+      //   console.log(p);
+
+      //   // should be a common function
+      //   document.getElementById("transaction-container").appendChild(p);
+    } else if (amount <= 0) {
+      alert("Donation amount cannot be negative");
+    } else if (isNaN(amount)) {
+      alert("Please input number only");
+    } else {
+      alert("Donation amount is exceeding current balance");
+    }
   });
